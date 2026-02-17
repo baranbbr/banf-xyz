9c7f21d57de9c83ac9af0f8d4c7f6c7e0f53b4c3
baranbbr <baranbaburisme@hotmail.com>
2026-02-17 23:57:40 +0000

    layout and customise wip

---
+++ app/components/projects.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+export function Projects() {
+	return (
+		<div>
+			<h1 className='font-semibold text-2xl mb-8 tracking-tighter'>
+				Projects
+			</h1>
+		</div>
+	)
+}
